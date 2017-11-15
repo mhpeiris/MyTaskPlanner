c4aa4439607697675514a4707accee7b6b2e77aa
ibrahimjaafar <ibrahimjaafar@live.com>
2017-11-15 10:17:24 -0500

    Minor changes

--- app/src/main/java/com/example/hareen/mytaskplanner/ScheduleActivity.java
+++ app/src/main/java/com/example/hareen/mytaskplanner/ScheduleActivity.java
@@ -4,7 +4,6 @@ package com.example.hareen.mytaskplanner;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
